735c6ec00c8a398f746d220c92e931338312a010
jatin510 <jatin6972@gmail.com>
2023-01-06 00:35:50 +0530

    added sentry

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import express from 'express';
 import cors from 'cors';
 
+import * as Sentry from '@sentry/node';
+import * as Tracing from '@sentry/tracing';
+
 import { config } from './config';
 
 const port = config.PORT;
@@ -8,8 +11,26 @@ const port = config.PORT;
 const app = express();
 app.use(cors());
 
+Sentry.init({
+  dsn: config.SENTRY_DSN,
+  integrations: [
+    new Sentry.Integrations.Http({ tracing: true }),
+    new Tracing.Integrations.Express({ app }),
+  ],
+  tracesSampleRate: 1.0,
+});
+
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
+
 app.get('/health', (_req, res) =>
   res.json({ ok: true, message: 'health check working fine' })
 );
+
+app.use(Sentry.Handlers.errorHandler());
+app.use(function onError(err, req, res, next) {
+  res.statusCode = 500;
+  res.end(res.sentry + '\n');
+});
 
 app.listen(port, () => console.log(`starting server in port ${port}`));
